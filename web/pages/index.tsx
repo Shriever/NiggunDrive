6294cf023924cfafdf06ce49d104836f062cff92
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-01 15:47:44 -0400

    configured tailwindcss

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+      <h1 className="border-black text-lg bg-gray-900" >works</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
