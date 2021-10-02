e23def9b77b4c873099d67233dbedc01a6402168
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-01 20:56:36 -0400

    added Links

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const LOGGED_IN = false;
 
@@ -8,25 +9,31 @@ const Navbar = () => {
       <div className='max-w-6xl mx-auto px-4'>
         <div className='flex space-x-7 justify-between'>
           <div>
-            <a href='#' className='flex items-center py-4 px-2'>
-              <span className='font-semibold text-gray-600 text-2xl'>
-                NiggunDrive
-              </span>
-            </a>
+            <Link href='/'>
+              <a href='#' className='flex items-center py-4 px-2'>
+                <span className='font-semibold text-gray-600 text-2xl'>
+                  NiggunDrive
+                </span>
+              </a>
+            </Link>
           </div>
           <div className='flex items-center space-x-3'>
-            <a
-              href='#'
-              className='py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300'
-            >
-              {LOGGED_IN ? 'Logout' : 'Login'}
-            </a>
-            <a
-              href='#'
-              className='py-2 px-2 font-medium rounded bg-green-500 text-white hover:bg-green-400 transition duration-300'
-            >
-              {LOGGED_IN ? 'Upload' : 'Register'}
-            </a>
+            <Link href='/login'>
+              <a
+                href='#'
+                className='py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300'
+              >
+                {LOGGED_IN ? 'Logout' : 'Login'}
+              </a>
+            </Link>
+            <Link href='/'>
+              <a
+                href='#'
+                className='py-2 px-2 font-medium rounded bg-green-500 text-white hover:bg-green-400 transition duration-300'
+              >
+                {LOGGED_IN ? 'Upload' : 'Register'}
+              </a>
+            </Link>
           </div>
         </div>
       </div>
@@ -34,4 +41,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar
+export default Navbar;
--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -5,7 +5,7 @@ const login = () => {
     return (
       <Wrapper>
         <h1 className='text-center text-3xl mt-4 mb-3 rounded'>Admin Login</h1>
-        <div className='max-w-sm shadow-xl mx-auto  pt-2 flex flex-col items-center p-6 py-8'>
+        <div className='max-w-md lg:max-w-sm shadow-xl mx-auto  pt-2 flex flex-col items-center p-6 py-8'>
           <input
             type='email'
             placeholder='Email Address'
@@ -16,7 +16,7 @@ const login = () => {
             placeholder='Password'
             className='w-4/5 mx-auto my-3 border p-3 focus:outline-none focus:border-green-500 rounded'
           />
-          <button className='w-100% mt-4 py-2 px-2 font-medium rounded bg-green-500 text-white hover:bg-green-400 transition duration-300'>
+          <button className='mt-4 py-4 px-6 font-medium rounded bg-green-500 text-white hover:bg-green-400 transition duration-300'>
             LOG IN NOW
           </button>
         </div>
