3acf4cfce3d0716e12129ff07cef494a3a8b3d59
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-01 20:47:04 -0400

    styled login page

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -3,13 +3,25 @@ import Wrapper from '../components/Wrapper'
 
 const login = () => {
     return (
-        <Wrapper>
-            
-            <h1>Admin Login</h1>
-            <input type="text" />
-            <input type="password" />
-        </Wrapper>
-    )
+      <Wrapper>
+        <h1 className='text-center text-3xl mt-4 mb-3 rounded'>Admin Login</h1>
+        <div className='max-w-sm shadow-xl mx-auto  pt-2 flex flex-col items-center p-6 py-8'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            className='w-4/5 mx-auto my-3 border p-3 focus:outline-none focus:border-green-500 rounded'
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            className='w-4/5 mx-auto my-3 border p-3 focus:outline-none focus:border-green-500 rounded'
+          />
+          <button className='w-100% mt-4 py-2 px-2 font-medium rounded bg-green-500 text-white hover:bg-green-400 transition duration-300'>
+            LOG IN NOW
+          </button>
+        </div>
+      </Wrapper>
+    );
 }
 
 export default login
