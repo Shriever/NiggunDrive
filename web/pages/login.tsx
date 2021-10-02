218c1376d5539a90cedc30b480bc006eb0d92e1c
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-01 20:07:37 -0400

    created wrapper component

---
+++ web/pages/login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Wrapper from '../components/Wrapper'
+
+const login = () => {
+    return (
+        <Wrapper>
+            
+            <h1>Admin Login</h1>
+            <input type="text" />
+            <input type="password" />
+        </Wrapper>
+    )
+}
+
+export default login
