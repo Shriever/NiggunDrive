7358204ca12c094a1e584312675327fac9cc99cc
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-05 16:48:16 -0400

    added header to index

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -7,6 +7,9 @@ import Wrapper from '../components/Wrapper';
 const Home: NextPage = () => {
   return (
     <Wrapper>
+      <h1 className='text-center text-3xl mt-5 mb-3 rounded font-semibold'>
+        Niggunim
+      </h1>
       <div className='mx-auto max-w-md mt-6'>
         {[0, 1, 2].map((_, idx) => (
           <Niggun key={idx} />
