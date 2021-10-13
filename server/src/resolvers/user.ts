5db661ff1c80581427e82a84f0e676d521dace0b
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-12 21:00:12 -0700

    added register mutation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -10,23 +10,24 @@ import {
 import { MyContext } from '../types';
 import { User } from '../entities/User';
 import { UsernamePasswordInput } from './UsernamePasswordInput';
+import { hash } from 'argon2';
 
 @ObjectType()
 class FieldError {
-    @Field()
-    field: string;
+  @Field()
+  field: string;
 
-    @Field()
-    message: string;
+  @Field()
+  message: string;
 }
 
 @ObjectType()
 class UserResponse {
-    @Field(() => [FieldError], {nullable: true})
-    errors?: FieldError[]
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
 
-    @Field(() => User, {nullable: true})
-    user?: User;
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver(User)
@@ -41,7 +42,56 @@ export class UserResolver {
   }
 
   @Mutation(() => UserResponse)
-  async register(@Arg("options") options: UsernamePasswordInput, @Ctx() {req}: MyContext) {
-      const errors
+  async register(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { req }: MyContext
+  ): Promise<UserResponse> {
+    const { email, password } = options;
+    if (email.length < 3) {
+      return {
+        errors: [
+          {
+            field: 'Email',
+            message: 'Email must be at least 3 characters long.',
+          },
+        ],
+      };
+    }
+
+    if (password.length < 3) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Password must be at least 3 characters long.',
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await hash(password);
+    try {
+      const user = await User.create({
+        email,
+        password: hashedPassword,
+      }).save();
+
+      req.session.userId = user.id;
+
+      return { user };
+    } catch (error) {
+      if (error.detail.includes('already exists')) {
+        return {
+          errors: [
+            {
+              field: 'email',
+              message: 'An account already exists with that email address',
+            },
+          ],
+        };
+      }
+    }
+    return { errors: [{field: 'register', message: 'An unknown error has occurred'}]};
+    }
   }
-}
+
